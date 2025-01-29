fca3fe47e2d6dd375027985665efee441d4ce298
Dener Garcia <dener.criarbr@gmail.com>
2025-01-29 13:44:08 -0300

    fix: change devAddres SD31 to SD41

--- src/devices/devicesList.js
+++ src/devices/devicesList.js
@@ -3,8 +3,8 @@ const devicesRemoteIo = [
     { val_workcenter: 'SD21', machine: "twi_machine", devAddr: 'FF97867A' },
     { val_workcenter: 'SD22', machine: "twi_machine", devAddr: 'FFA38B17' },
     { val_workcenter: 'SD23', machine: "twi_machine", devAddr: 'FFA38AFF' },
-    { val_workcenter: 'SD31', machine: "twi_machine", devAddr: 'FFA38B06' },
-    { val_workcenter: 'SD41', machine: "twi_machine", devAddr: 'FF978689' },
+    { val_workcenter: 'SD31', machine: "twi_machine", devAddr: 'FF978689' },
+    { val_workcenter: 'SD41', machine: "twi_machine", devAddr: 'FFA38B06' },
     { val_workcenter: 'SD62', machine: "twi_machine", devAddr: 'FFA38B0F' },
     { val_workcenter: 'SD63', machine: "twi_machine", devAddr: 'FF978681' },
   ]
